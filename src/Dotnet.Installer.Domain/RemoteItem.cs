9a6e405dd63d1837bf4acc650f47a417edd4e2c7
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2024-01-22 16:25:54 -0300

    Upgrade to .NET 7

--- src/Dotnet.Installer.Domain/RemoteItem.cs
+++ src/Dotnet.Installer.Domain/RemoteItem.cs
@@ -5,18 +5,18 @@ namespace Dotnet.Installer.Domain;
 
 public class RemoteItem
 {
-    public string Key { get; set; }
+    public required string Key { get; set; }
 
-    public string Component { get; set; }
+    public required string Component { get; set; }
 
-    public Uri Url { get; set; }
+    public required Uri Url { get; set; }
 
-    public string Sha256 { get; set; }
+    public required string Sha256 { get; set; }
 
     [JsonConverter(typeof(DotnetVersionJsonConverter))]
-    public DotnetVersion Version { get; set; }
+    public required DotnetVersion Version { get; set; }
     
-    public IEnumerable<string> Locations { get; set; }
+    public required IEnumerable<string> Locations { get; set; }
 
-    public IEnumerable<string> Dependencies { get; set; }
+    public required IEnumerable<string> Dependencies { get; set; }
 }
