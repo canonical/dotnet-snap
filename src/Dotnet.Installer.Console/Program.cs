21816b48267c6eb38f59da38374d4ce960d0c8a9
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2024-05-27 14:35:49 -0300

    Removed dependency injection engine

--- src/Dotnet.Installer.Console/Program.cs
+++ src/Dotnet.Installer.Console/Program.cs
@@ -1,35 +1,24 @@
 ﻿using System.CommandLine;
 using Dotnet.Installer.Console.Commands;
-using Dotnet.Installer.Core.Services.Contracts;
 using Dotnet.Installer.Core.Services.Implementations;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Dotnet.Installer.Console;
 
 class Program
 {
-    static async Task Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        var serviceProvider = new ServiceCollection()
-            .AddSingleton<IFileService, FileService>()
-            .AddSingleton<IManifestService, ManifestService>()
-            .AddSingleton<ILimitsService, LimitsService>()
-            .AddSingleton(serviceProvider =>
-            {
-                var fileService = serviceProvider.GetRequiredService<IFileService>();
-                var manifestService = serviceProvider.GetRequiredService<IManifestService>();
-                var limitsService = serviceProvider.GetRequiredService<ILimitsService>();
-                return new RootCommand(".NET Installer command-line tool")
-                {
-                    new ListCommand(manifestService),
-                    new InstallCommand(fileService, limitsService, manifestService),
-                    new RemoveCommand(fileService, manifestService),
-                    new UpdateCommand(fileService, limitsService, manifestService)
-                };
-            })
-            .BuildServiceProvider();
-
-        var rootCommand = serviceProvider.GetRequiredService<RootCommand>();
+        var fileService = new FileService();
+        var manifestService = new ManifestService();
+        var limitsService = new LimitsService(fileService);
+        
+        var rootCommand = new RootCommand(".NET Installer command-line tool")
+        {
+            new ListCommand(manifestService),
+            new InstallCommand(fileService, limitsService, manifestService),
+            new RemoveCommand(fileService, manifestService),
+            new UpdateCommand(fileService, limitsService, manifestService)
+        };
 
         await rootCommand.InvokeAsync(args);
     }
