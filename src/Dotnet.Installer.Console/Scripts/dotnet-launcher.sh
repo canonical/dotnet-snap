a80e26d101530f81a1d538cd3fa76f644ae3d99f
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2024-06-05 16:39:54 -0300

    Add first-time usage checker in launcher script

--- src/Dotnet.Installer.Console/Scripts/dotnet-launcher.sh
+++ src/Dotnet.Installer.Console/Scripts/dotnet-launcher.sh
@@ -5,6 +5,13 @@ if [ "$1" = "installer" ]; then
     "$SNAP"/Dotnet.Installer.Console "${@: 2}"
 else
     # User is not invoking the .NET installer tool.
+    
+    # Check whether there is any .NET components installed.
+    # If not, we install the latest SDK by default.
+    if [ "$(jq length "${SNAP_COMMON}/snap/manifest.json")" -eq "0" ]; then
+      "$SNAP"/Dotnet.Installer.Console install sdk latest
+    fi 
+    
     # Pass-through all arguments to .NET host.
     "$DOTNET_INSTALL_DIR"/dotnet "$@"
 fi
