99243d06f7b48b510ee885df8ebe15f188bee6ff
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2024-06-11 18:38:59 -0300

    Add process elevation logic to the launcher

--- src/Dotnet.Installer.Console/Scripts/dotnet-launcher.sh
+++ src/Dotnet.Installer.Console/Scripts/dotnet-launcher.sh
@@ -1,15 +1,48 @@
 #!/bin/bash
+set -euo pipefail
+
+run_elevated() {
+    # Pass second argument onwards to .NET installer tool.
+    CommandToExecute=$1
+    DotnetInstallDir=$DOTNET_INSTALL_DIR
+    ServerUrl=$SERVER_URL
+    
+    # If the third argument is either install or remove, we need to elevate the process.
+    ProcessEuid=$("$SNAP"/Dotnet.Installer.Console environment | grep "Process+EffectiveUserId" | cut -d= -f2)
+    
+    if [ "$ProcessEuid" -eq "0" ]; then
+        $CommandToExecute
+    else
+        if [ -e "/usr/bin/pkexec" ]; then
+            # shellcheck disable=SC2086
+            pkexec DOTNET_INSTALL_DIR=$DotnetInstallDir SERVER_URL=$ServerUrl $CommandToExecute
+        else
+            # shellcheck disable=SC2086
+            sudo DOTNET_INSTALL_DIR=$DotnetInstallDir SERVER_URL=$ServerUrl $CommandToExecute
+        fi
+    fi
+}
 
 if [ "$1" = "installer" ]; then
     # Pass second argument onwards to .NET installer tool.
-    "$SNAP"/Dotnet.Installer.Console "${@: 2}"
+    CommandToExecute="$SNAP/Dotnet.Installer.Console ${*: 2}"
+    
+    need_elevation_commands=("install" "remove")
+        
+    # shellcheck disable=SC2076
+    if [[ "${need_elevation_commands[*]}" =~ "$2" ]]; then
+        run_elevated "$CommandToExecute"
+    else
+        $CommandToExecute
+    fi
 else
     # User is not invoking the .NET installer tool.
     
     # Check whether there is any .NET components installed.
     # If not, we install the latest SDK by default.
     if [ "$(jq length "${SNAP_COMMON}/snap/manifest.json")" -eq "0" ]; then
-      "$SNAP"/Dotnet.Installer.Console install sdk latest
+        CommandToExecute="$SNAP/Dotnet.Installer.Console install sdk latest"
+        run_elevated "$CommandToExecute"
     fi 
     
     # Pass-through all arguments to .NET host.
