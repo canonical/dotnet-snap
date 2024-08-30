b0af00d202f1ade8aac6eca6c24dd8549f98c2c7
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2024-08-30 14:15:13 -0300

    chore: remove unused methods from FileService

--- src/Dotnet.Installer.Core/Services/Contracts/IFileService.cs
+++ src/Dotnet.Installer.Core/Services/Contracts/IFileService.cs
@@ -7,8 +7,5 @@ public interface IFileService
     void UninstallSystemdMountUnit(string unitName);
     void InstallSystemdPathUnit(string snapName);
     void UninstallSystemdPathUnit(string snapName);
-    IDictionary<string, string> ResolveMountPoints(string root, IEnumerable<string> targets);
-    Task ExecuteMountPoints(string root, IDictionary<string, string> mountPoints);
-    Task RemoveMountPoints(string root, IDictionary<string, string> mountPoints);
     void RemoveEmptyDirectories(string root);
 }
