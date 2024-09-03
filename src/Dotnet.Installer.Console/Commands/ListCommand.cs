67e5944e54296463cb1d101c10f08fcd8d858dec
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2024-09-03 18:47:28 -0300

    feat: updated list command with new versions table

--- src/Dotnet.Installer.Console/Commands/ListCommand.cs
+++ src/Dotnet.Installer.Console/Commands/ListCommand.cs
@@ -1,16 +1,21 @@
 ﻿using System.CommandLine;
-using System.Text;
+using Dotnet.Installer.Core.Models;
 using Dotnet.Installer.Core.Services.Contracts;
+using Dotnet.Installer.Core.Types;
 using Spectre.Console;
 
 namespace Dotnet.Installer.Console.Commands;
 
 public class ListCommand : Command
 {
+    private readonly IFileService _fileService;
     private readonly IManifestService _manifestService;
 
-    public ListCommand(IManifestService manifestService) : base("list", "List installed and available .NET versions")
+    public ListCommand(
+        IFileService fileService,
+        IManifestService manifestService) : base("list", "List installed and available .NET versions")
     {
+        _fileService = fileService ?? throw new ArgumentNullException(nameof(fileService));
         _manifestService = manifestService ?? throw new ArgumentNullException(nameof(manifestService));
 
         this.SetHandler(Handle);
@@ -21,28 +26,55 @@ public class ListCommand : Command
         try
         {
             await _manifestService.Initialize();
-            var tree = new Tree("Available Components");
 
-            foreach (var versionGroup in _manifestService.Merged.GroupBy(c => c.MajorVersion))
+            var table = new Table();
+
+            table.AddColumn(new TableColumn("Version"));
+            table.AddColumn(new TableColumn(".NET Runtime"));
+            table.AddColumn(new TableColumn("ASP.NET Core Runtime"));
+            table.AddColumn(new TableColumn("SDK"));
+
+            foreach (var majorVersionGroup in _manifestService.Merged
+                         .GroupBy(c => c.MajorVersion)
+                         .OrderBy(c => c.Key))
             {
-                var majorVersionNode = tree.AddNode($".NET {versionGroup.Key}");
+                var components = new Dictionary<string, (bool Installed, DotnetVersion? Version)>();
 
-                foreach (var component in versionGroup)
+                var installedComponents = majorVersionGroup.Where(c => c.Installation is not null);
+                foreach (var installedComponent in installedComponents)
                 {
-                    var stringBuilder = new StringBuilder();
+                    var version = installedComponent.GetDotnetVersion(_manifestService, _fileService);
+                    components[installedComponent.Name] = (Installed: true, Version: version);
+                }
 
-                    stringBuilder.Append(component.Description);
+                var dotNetRuntimeStatus = ComponentStatus(Constants.DotnetRuntimeComponentName, majorVersionGroup.Key);
+                var aspNetCoreRuntimeStatus = ComponentStatus(Constants.AspnetCoreRuntimeComponentName, majorVersionGroup.Key);
+                var sdkStatus = ComponentStatus(Constants.SdkComponentName, majorVersionGroup.Key);
 
-                    if (component.Installation is not null)
+                table.AddRow($".NET {majorVersionGroup.Key.ToString()}", dotNetRuntimeStatus, aspNetCoreRuntimeStatus, sdkStatus);
+                continue;
+
+                string ComponentStatus(string key, int majorVersion)
+                {
+                    string status;
+                    var available = _manifestService.Remote.Any(c => c.Name == key && c.MajorVersion == majorVersion);
+
+                    if (components.TryGetValue(key, out var component))
                     {
-                        stringBuilder.Append("[bold green]Installed :check_mark_button:[/]");
+                        status = $"[bold green]Installed [[{component.Version}]][/]";
                     }
-                    
-                    majorVersionNode.AddNode($"{component.Description}");
+                    else
+                    {
+                        status = available
+                            ? "[bold blue]Available[/]"
+                            : "[bold grey]Unavailable[/]";
+                    }
+
+                    return status;
                 }
             }
-            
-            AnsiConsole.Write(tree);
+
+            AnsiConsole.Write(table);
         }
         catch (ApplicationException ex)
         {
