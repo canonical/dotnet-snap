60e20d2db9fd45c6d8c1320e8e47268064f367f5
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2024-10-02 10:46:18 -0300

    feat: suppress systemd output to console

--- src/Dotnet.Installer.Core/Types/Terminal.cs
+++ src/Dotnet.Installer.Core/Types/Terminal.cs
@@ -6,20 +6,36 @@ public static class Terminal
 {
     public static async Task<int> Invoke(string program, params string[] arguments)
     {
+        return await Invoke(program, options: null, arguments);
+    }
+
+    public static async Task<int> Invoke(string program, InvocationOptions? options = default,
+        params string[] arguments)
+    {
+        options ??= InvocationOptions.Default;
+
         var process = new Process();
 
         process.StartInfo.FileName = program;
-        
+
         foreach (var argument in arguments)
             process.StartInfo.ArgumentList.Add(argument);
-        
+
         process.StartInfo.CreateNoWindow = true;
-        process.StartInfo.RedirectStandardOutput = false;
-        process.StartInfo.RedirectStandardError = false;
+        process.StartInfo.RedirectStandardOutput = options.RedirectStandardOutput;
+        process.StartInfo.RedirectStandardError = options.RedirectStandardError;
 
         process.Start();
         await process.WaitForExitAsync();
 
         return process.ExitCode;
+    }
+
+    public class InvocationOptions
+    {
+        public static InvocationOptions Default => new();
+
+        public bool RedirectStandardOutput { get; set; } = false;
+        public bool RedirectStandardError { get; set; } = false;
     }
 }
