488f941b77c8452b859cbefaf82a486bb7f67254
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2025-01-27 18:04:58 -0300

    feat: add 'edge' flag to snap Install command

--- src/Dotnet.Installer.Core/Services/Contracts/ISnapService.cs
+++ src/Dotnet.Installer.Core/Services/Contracts/ISnapService.cs
@@ -6,7 +6,7 @@ namespace Dotnet.Installer.Core.Services.Contracts;
 public interface ISnapService : IDisposable
 {
     bool IsSnapInstalled(string name, CancellationToken cancellationToken = default);
-    Task<Terminal.InvocationResult> Install(string name, CancellationToken cancellationToken = default);
+    Task<Terminal.InvocationResult> Install(string name, bool edge = false, CancellationToken cancellationToken = default);
     Task<Terminal.InvocationResult> Remove(string name, bool purge = false, CancellationToken cancellationToken = default);
     Task<IImmutableList<SnapInfo>> GetInstalledSnaps(CancellationToken cancellationToken = default);
     Task<SnapInfo?> GetInstalledSnap(string name, CancellationToken cancellationToken = default);
