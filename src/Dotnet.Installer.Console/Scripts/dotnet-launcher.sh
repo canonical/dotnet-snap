cb5a09557f9fa99f34eaba1fada0a0e5dd172c4e
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2025-02-11 12:04:33 -0300

    chore: move default LTS install to first execution
    Having snapd install the SDK content snap as a requirement is problematic because, if a user removes that component and installs a different one, the default LTS SDK content snap will be reinstalled on the next installer snap refresh.

---
+++ src/Dotnet.Installer.Console/Scripts/dotnet-launcher.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Check for installed .NET components
+manifest_path="$SNAP_COMMON/snap/manifest.json"
+
+if [[ $("$SNAP"/usr/bin/jq 'length' "$manifest_path") -eq 0 ]]; then
+    echo "Looks like you don't yet have a .NET SDK or Runtime installed."
+    echo "I am downloading and installing the latest LTS SDK for you to use. It should only be a few moments."
+
+    if ! "$SNAP/Scripts/dotnet-installer-launcher.sh" install sdk lts; then
+        echo "Could not install the latest .NET SDK. Please check your credentials or run this command with sudo."
+        exit 255
+    fi
+fi
+
+# Pass-through all arguments to .NET host
+dotnet_executable="$DOTNET_INSTALL_DIR/dotnet"
+"$dotnet_executable" "$@"
