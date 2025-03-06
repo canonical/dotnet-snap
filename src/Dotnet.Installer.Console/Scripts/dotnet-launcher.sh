2674df06f4bf0cb51776ec5fc082435eb70323aa
Dominik Viererbe <dominik.viererbe@canonical.com>
2025-03-06 15:57:50 +0200

    chore: minor wording improvement of first-use installation message
    Signed-off-by: Dominik Viererbe <dominik.viererbe@canonical.com>

--- src/Dotnet.Installer.Console/Scripts/dotnet-launcher.sh
+++ src/Dotnet.Installer.Console/Scripts/dotnet-launcher.sh
@@ -7,7 +7,7 @@ manifest_path="$SNAP_COMMON/snap/manifest.json"
 
 if [[ $("$SNAP"/usr/bin/jq 'length' "$manifest_path") -eq 0 ]]; then
     echo "Looks like you don't yet have a .NET SDK or Runtime installed."
-    echo "I am downloading and installing the latest LTS SDK for you to use. It should only be a few moments."
+    echo "Downloading and installing the latest LTS SDK for you to use. It should only take a few moments."
 
     if ! "$SNAP/Scripts/dotnet-installer-launcher.sh" install sdk lts; then
         echo "Could not install the latest LTS .NET SDK. Please check your credentials or run this command with sudo."
